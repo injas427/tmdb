99ad0e22eb2612dcdd8ee4a4cfbc20fba504b054
Injas MTP <injas.mtp@qvantel.com>
2024-01-17 10:55:44 +0530

    updated readme and initialization message

--- initialize.ts
+++ initialize.ts
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log('=========== Initializing Project ===========');
+console.log('\x1b[0;32m=========== Initializing Project ===========\x1b[0m');
 
 const jsonData = {
   secure: {
@@ -19,8 +19,13 @@ const filePath = path.join(__dirname, json_path);
 
 fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), err => {
   if (err) {
-    console.error('Error creating JSON file:', err);
+    console.error('\x1b[0;31mError creating JSON file:\x1b[0m', err);
   } else {
-    console.log('=========== Project Initializing Done ===========');
+    console.log(
+      '\x1b[0;32m=========== Project Initializing Done ===========\x1b[0m',
+    );
+    console.log(
+      `\x1b[0;36m=========== Please update your API key at ${json_path}  ===========\x1b[0m`,
+    );
   }
 });
